b73db5faa2dbff2616c859bba4a91e6f2817356e
vitor-o-s <vitor-o-s>
2023-05-10 22:45:35 +0000

    Final aulas + inicio exercicios

---
+++ dia02/ListaExercicios.sql
@@ -0,0 +1,15 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC
+-- MAGIC 1. Selecione todos os clientes paulistanos.
+-- MAGIC 2. Selecione todos os clientes paulistas.
+-- MAGIC 3. Selecione todos os vendedores cariocas e paulistas.
+-- MAGIC 4. Selecione produtos de perfumaria e bebes com altura maior que 5cm.
+-- MAGIC 5. Selecione os pedidos com mais de um item.
+-- MAGIC 6. Selecione os pedidos que o frete é mais caro que o item.
+-- MAGIC 7. Lista de pedidos que ainda não foram enviados.
+-- MAGIC 8. Lista de pedidos que foram entregues com atraso.
+-- MAGIC 9. Lista de pedidos que foram entregues com 2 dias de antecedência.
+-- MAGIC 10. Selecione os pedidos feitos em dezembro de 2017 e entregues com atraso.
+-- MAGIC 11. Selecione os pedidos com avaliação maior ou igual que 4.
+-- MAGIC 12. Selecione pedidos pagos com cartão de crédito com duas ou mais parcelas menores que R$40,00.
--- dia02/SELECT.sql
+++ dia02/SELECT.sql
@@ -97,3 +97,138 @@ SELECT date_diff('2023-06-01','2023-01-01'), months_between('2023-06-01','2023-0
 -- COMMAND ----------
 
 SELECT idPedido, idCliente, dtPedido, dtEntregue, date_diff(dtEntregue, dtPedido) AS diasENtreEntregaPedido FROM silver.olist.pedido LIMIT 1000
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC # SELECT FROM WHERE (DAY-2/A6)
+
+-- COMMAND ----------
+
+SELECT * FROM silver.olist.item_pedido WHERE vlPreco >= 500
+
+-- COMMAND ----------
+
+SELECT * FROM silver.olist.item_pedido WHERE vlFrete > vlPreco
+
+-- COMMAND ----------
+
+SELECT * FROM silver.olist.item_pedido WHERE vlPreco >= 100 AND vlFrete > vlPreco
+
+-- COMMAND ----------
+
+SELECT * FROM silver.olist.produto WHERE descCategoria = 'pet_shop' OR descCategoria = 'telefonia' OR descCategoria = 'bebes'
+
+-- COMMAND ----------
+
+SELECT * FROM silver.olist.produto WHERE descCategoria IN ('pet_shop', 'telefonia', 'bebes')
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Pedidos de jan/2017
+SELECT idPedido, idCliente, descSituacao, dtPedido, date(dtPedido)
+FROM silver.olist.pedido
+WHERE date(dtPedido) >= '2017-01-01' AND date(dtPedido) <= '2017-01-31'
+
+
+-- SELECT idPedido, idCliente, descSituacao, dtPedido
+-- FROM silver.olist.pedido
+-- WHERE dtPedido >= '2017-01-01' AND dtPedido <= '2017-01-31'
+-- Como os dados estão em timestamp a consulta exclui dados do dia 31 após 00hrs
+
+-- COMMAND ----------
+
+SELECT idPedido, idCliente, descSituacao, dtPedido, date(dtPedido)
+FROM silver.olist.pedido
+WHERE year(dtPedido) = 2017 AND month(dtPedido) = 1
+
+-- COMMAND ----------
+
+SELECT *
+FROM silver.olist.pedido
+WHERE year(dtPedido) = 2017 AND (month(dtPedido) = 1 OR month(dtPedido) = 6)
+-- Parenteses podem alterar o resultado
+
+-- COMMAND ----------
+
+SELECT *
+FROM silver.olist.pedido
+WHERE year(dtPedido) = 2017 AND month(dtPedido) IN (1, 6)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC # SELECT CASE (DAY-2/A7)
+
+-- COMMAND ----------
+
+SELECT *,
+  CASE WHEN vlPreco <= 100 THEN '000 -| 100'
+       WHEN vlPreco <= 200 THEN '101 -| 200'
+       WHEN vlPreco <= 300 THEN '201 -| 300'
+       ELSE '>300'
+  END AS faixaPreco
+FROM silver.olist.item_pedido
+
+-- COMMAND ----------
+
+SELECT *,
+  CASE WHEN vlFrete / (vlFrete + vlPreco) = 0 THEN 'Frete Gratuito'
+       WHEN vlFrete / (vlFrete + vlPreco) <= 0.2 THEN 'Frete Baixo'
+       WHEN vlFrete / (vlFrete + vlPreco) <= 0.4 THEN 'Frete Médio'
+       ELSE 'Frete Alto'
+  END AS descFrete
+FROM silver.olist.item_pedido
+
+-- COMMAND ----------
+
+SELECT *,
+  CASE WHEN descUF IN ('SC', 'PR', 'RS') THEN 'Sul'
+       WHEN descUF IN ('SP', 'MG', 'RJ', 'ES') THEN 'Sudeste'
+       WHEN descUF IN ('MS', 'MT', 'GO', 'DF') THEN 'Centro-Oeste'
+       WHEN descUF IN ('AC', 'RO', 'AM', 'RR', 'PA', 'AP', 'TO') THEN 'Norte'
+       ELSE 'Nordeste'
+  END AS descRegiao
+FROM silver.olist.cliente
+
+-- COMMAND ----------
+
+SELECT *,
+  CASE WHEN descUF = 'SP' THEN 'Paulista'
+       WHEN descUF = 'RJ' THEN 'Fluminense'
+       WHEN descUF = 'MG' THEN 'Mineiro'
+       WHEN descUF = 'AC' THEN 'Acreano'
+       WHEN descUF = 'BA' THEN 'Baiano'
+       ELSE 'Não Mapeado'
+  END AS descNacionalidade
+FROM silver.olist.vendedor
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC # SELECT CASE FROM WHERE (DAY-2/A8)
+
+-- COMMAND ----------
+
+SELECT *, 
+  CASE
+    WHEN vlPreco <= 100 THEN '000 -| 100'
+    WHEN vlPreco <= 200 THEN '101 -| 200'
+    WHEN vlPreco <= 300 THEN '201 -| 300'
+    WHEN vlPreco <= 400 THEN '301 -| 400'
+    WHEN vlPreco <= 500 THEN '401 -| 500'
+    WHEN vlPreco <= 600 THEN '501 -| 600'
+    WHEN vlPreco <= 700 THEN '601 -| 700'
+    WHEN vlPreco <= 800 THEN '701 -| 800'
+    WHEN vlPreco <= 900 THEN '801 -| 900'
+    WHEN vlPreco <= 1000 THEN '901 -| 1000'
+    ELSE '+100' 
+  END AS fxPreco,
+  CASE 
+    WHEN vlFrete / (vlFrete + vlPreco) = 0 THEN 'Frete Gratuito'
+    WHEN vlFrete / (vlFrete + vlPreco) <= 0.2 THEN 'Frete Baixo'
+    WHEN vlFrete / (vlFrete + vlPreco) <= 0.4 THEN 'Frete Médio'
+    ELSE 'Frete Alto'
+  END AS fxFrete
+FROM silver.olist.item_pedido
+WHERE vlPreco >= 50
